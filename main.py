ab90e4f60084666c8392f2fa04b9c15d16189936
hibiscus22 <miguelmlozano@gmail.com>
2024-06-22 18:35:14 +0200

    Add file browse, main code structure

--- main.py
+++ main.py
@@ -2,24 +2,35 @@ import PySimpleGUI as sg
 import cv2
 import numpy as np
 
-image_viewer_brightfield = [[sg.Text("Brightfield")], [sg.Image(key="-IMAGE_BF-")]]
 
-image_viewer_stain = [[sg.Text("Stain")], [sg.Image(key="-IMAGE_DYE-")]]
+def main() -> None:
 
-layout = [
-    [
-        sg.Column(image_viewer_brightfield),
-        sg.VSeparator(),
-        sg.Column(image_viewer_stain),
-    ],
-    [sg.Radio("Binary Masks", "Radio", size=(10, 2), key="-MASK-")],
-    [sg.Radio("Ternary Masks", "Radio", size=(10, 2), key="-MASK3-")],
-    [sg.Radio("Continuous", "Radio", size=(10, 2), key="-CONTINUOUS-")],
-]
+    image_viewer_brightfield = [
+        [sg.Text("Brightfield")],
+        [sg.Image(key="-IMAGE_BF-")],
+        [sg.FileBrowse()],
+    ]
 
-window = sg.Window("Necrosis Detector", layout)
+    image_viewer_stain = [[sg.Text("Stain")], [sg.Image(key="-IMAGE_DYE-")]]
 
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED:
-        break
+    layout = [
+        [
+            sg.Column(image_viewer_brightfield),
+            sg.VSeparator(),
+            sg.Column(image_viewer_stain),
+        ],
+        [sg.Radio("Binary Masks", "Radio", size=(10, 2), key="-MASK-")],
+        [sg.Radio("Ternary Masks", "Radio", size=(10, 2), key="-MASK3-")],
+        [sg.Radio("Continuous", "Radio", size=(10, 2), key="-CONTINUOUS-")],
+    ]
+
+    window = sg.Window("Necrosis Detector", layout)
+
+    while True:
+        event, _ = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+
+
+if __name__ == "__main__":
+    main()
